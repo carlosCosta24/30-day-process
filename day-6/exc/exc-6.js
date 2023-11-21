16ad276e9c227a763e514a59f4f60e1f4f78a67c
M0lt0 <bodymgahed@yahoo.com>
2023-11-21 19:43:39 -0300

    day -6 level 1

---
+++ day-6/exc/exc-6.js
@@ -0,0 +1,45 @@
+//Exercises: Level 1
+for(let i = 0; i < 10; i++){
+
+    console.log(`this is the ${i} *`);
+}
+
+
+let i=0;
+while(i <10){
+console.log(i);
+i++
+}
+
+let j = 0;
+do{
+    console.log(` ${ '#' }`);
+    j++
+}while(j<10);
+
+for(let i = 10; i >0; i--){
+
+    console.log(`this is the ${i} *`);
+}
+
+let k=10;
+while(i > 0){
+console.log(i);
+i--
+}
+
+let c = 10;
+do{
+    console.log(` ${ '#' } * ${c}`);
+    c--;
+}while(c > 0);
+
+
+for(let n =0 ; n< 50 ; n++){
+    console.log('************************************\n**************************************************');
+}
+
+
+for(let f = 0; f < 7 ; f++ ){
+    console.log(`${'#' * f} `);
+}
