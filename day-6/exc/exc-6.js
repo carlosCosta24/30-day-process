deb2df09a8501b73dea951539c57db5bc412e9c8
M0lt0 <bodymgahed@yahoo.com>
2023-11-23 10:24:43 -0300

    day -6 level 2

--- day-6/exc/exc-6.js
+++ day-6/exc/exc-6.js
@@ -39,7 +39,92 @@ for(let n =0 ; n< 50 ; n++){
     console.log('************************************\n**************************************************');
 }
 
+let hash = '#';
+for(let f = 1; f < 8 ; f++ ){
 
-for(let f = 0; f < 7 ; f++ ){
-    console.log(`${'#' * f} `);
+    console.log(`${hash}`);
+    hash += "#";
 }
+
+for(let x = 0; x <= 10 ; x++){
+    console.log(`${x} x ${x} = ${x*x}\n`)
+
+
+
+}
+
+console.log('i\t i^2\t i^3');
+for(let x = 0; x <= 10 ; x++){
+
+    console.log(`${x}\t ${ x*x}\t ${ x*x*x}\n`);
+
+
+
+}
+//Use for loop to iterate from 0 to 100 and print only even numbers
+for(let y=0 ; y <= 100; y++){
+    if( y % 2 ==0 ){
+        console.log(y);
+    }
+}
+
+//Use for loop to iterate from 0 to 100 and print only odd numbers
+
+console.log('************************************************************');
+for(let y=0 ; y <= 100; y++){
+    if( y % 2 !==0 ){
+        console.log(y);
+    }
+}
+console.log('************************************************************');
+//Use for loop to iterate from 0 to 100 and print only prime numbers
+for(let y=0 ; y <= 100; y++){
+    if( y % y ==0 && y % 3 == 0 ){
+        console.log(y);
+    }
+}
+console.log('************************************************************');
+//se for loop to iterate from 0 to 100 and print the sum of all numbers
+let theSum = 0;
+for(let y=0 ; y <= 100; y++){
+    theSum +=y;
+}
+console.log(`The sum of all numbers from 0 to 100 is ${theSum}`);
+console.log('************************************************************');
+//Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+let theEvenSum = 0;
+let theOddSum = 0;
+for(let y=0 ; y <= 100; y++){
+    if(y % 2 ==0){
+        theEvenSum +=y;
+    }else{
+        theOddSum += y;
+    }
+
+}
+let theArr = [];
+theArr.push(theEvenSum,theOddSum)
+console.log(`The sum of all evens from 0 to 100 is ${theEvenSum}. And the sum of all odds from 0 to 100 is ${theOddSum}.`);
+console.log('************************************************************');
+console.log(theArr);
+console.log('************************************************************');
+let randArr = [];
+for(let q = 0 ; q <= 5 ; q++ ){
+    let rand = Math.floor(Math.random()*10);
+    randArr.push(rand)
+};
+console.log(randArr);
+console.log('************************************************************');
+//Develop a small script which generate a six characters random id:
+let rand = Math.floor(Math.random()*10);
+let num =[1,2,3,4,5,6,7,8,9];
+let char = ['a','b','c','d','f','g'];
+let randString= [];
+for(let l = 0 ; l<= 6 ; l++){
+    randString.push(char[rand]);
+    randString.push(num[rand]);
+}
+console.log(randString);
+
+
+
