a1684e69841ee3f2fa5ee8d74f971d83639a839f
M0lt0 <bodymgahed@yahoo.com>
2024-01-10 18:00:49 -0300

    day 9 level 2

--- day-9/day-9.js
+++ day-9/day-9.js
@@ -1,4 +1,199 @@
 //Exercises: Level 1
+const allCountries = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bhutan',
+    'Bolivia',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Brazil',
+    'Brunei',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cape Verde',
+    'Central African Republic',
+    'Chad',
+    'Chile',
+    'China',
+    'Colombi',
+    'Comoros',
+    'Congo (Brazzaville)',
+    'Congo',
+    'Costa Rica',
+    "Cote d'Ivoire",
+    'Croatia',
+    'Cuba',
+    'Cyprus',
+    'Czech Republic',
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic',
+    'East Timor (Timor Timur)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Ethiopia',
+    'Fiji',
+    'Finland',
+    'France',
+    'Gabon',
+    'Gambia, The',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Greece',
+    'Grenada',
+    'Guatemala',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Honduras',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+    'Ireland',
+    'Israel',
+    'Italy',
+    'Jamaica',
+    'Japan',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    'Korea, North',
+    'Korea, South',
+    'Kuwait',
+    'Kyrgyzstan',
+    'Laos',
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macedonia',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands',
+    'Mauritania',
+    'Mauritius',
+    'Mexico',
+    'Micronesia',
+    'Moldova',
+    'Monaco',
+    'Mongolia',
+    'Morocco',
+    'Mozambique',
+    'Myanmar',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands',
+    'New Zealand',
+    'Nicaragua',
+    'Niger',
+    'Nigeria',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Romania',
+    'Russia',
+    'Rwanda',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Vincent',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia and Montenegro',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'Spain',
+    'Sri Lanka',
+    'Sudan',
+    'Suriname',
+    'Swaziland',
+    'Sweden',
+    'Switzerland',
+    'Syria',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania',
+    'Thailand',
+    'Togo',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Vatican City',
+    'Venezuela',
+    'Vietnam',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe'
+]
 const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -217,6 +412,63 @@ let russiaPosition = countries.findIndex((country) => {
 });
 console.log("ifgh0988ytdvsNDHBUIswrgh");
 console.log(russiaPosition)
+//Exercises: Level 2
+//Find the total price of products by chaining two or more array iterators(eg. arr.map(callback).filter(callback).reduce(callback))
+let totalPrice = products.map((product) => { return product.price; }).filter(ele => { if (ele !== "" && ele !== " ") { return ele } }).reduce((acc, curr) => { return acc + curr }, 0);
+console.log("ifgh0988ytdvsNDHBUIswrgh");
+console.log(totalPrice);
+//Find the sum of price of products using only reduce reduce(callback)
+let usingOnlyReduce = products.reduce((acc, prod) => {
+    if (prod.price !== "" && prod.price !== " ") {
+        return acc + parseInt(prod.price);
+    } else {
+        return acc
+    }
+}, 0);
+console.log("ifgh0988ytdvsNDHBUIswrgh");
+console.log(usingOnlyReduce);
+//Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
+
+function categorizeCountries(arr, pattern) {
+    let finalArr = [];
+    arr.filter((country) => {
+        if (country.toLowerCase().includes(pattern)) {
+
+            finalArr.push(country)
+        }
+    })
+    return finalArr;
+
+}
+console.log("ifgh0988ytdvsNDHBUIswrgh");
+console.log(categorizeCountries(allCountries, "br"));
+
+//Create a function which return an array of objects, which is the letter and the number of times the letter use to start with a name of a country.
+
+function arrOfObject(allCountries) {
+    let result = [];
+    allCountries.every((country) => {
+        let count = 0;
+        let pointer = 0;
+        let startWith = country.charAt(0).toLowerCase();
+        if (country.charAt(startWith) == country[pointer + 1].charAt(startWith)) {
+            count++;
+            pointer++
+        } else {
+            result.push({
+                letter: `${startWith}`,
+                number: `${count}`
+            });
+            count = 0;
+        }
+
+    });
+
+    return result
+
+
+}
+console.log(arrOfObject(allCountries));
 
 
 
