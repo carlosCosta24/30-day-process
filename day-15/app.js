d5fde381a0122e252dfa7117b72a5d0877839ec6
M0lt0 <bodymgahed@yahoo.com>
2024-01-30 20:06:35 -0300

    day 15 l 1

--- day-15/app.js
+++ day-15/app.js
@@ -1,1 +1,12 @@
-console.info("its working");
+//Exercises Level 1
+//Create an Animal class. The class will have name, age, color, legs properties and create different methods
+class Animal {
+    constructor(name, age, color, legs) {
+        name = this.name,
+            age = this.age,
+            color = this.color,
+            legs = this.legs
+    }
+
+}
+
