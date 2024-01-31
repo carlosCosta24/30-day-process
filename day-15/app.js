1a3859e09d6f525adb334510cfd11cff04385294
M0lt0 <bodymgahed@yahoo.com>
2024-01-31 17:51:42 -0300

    day 15 l 1

--- day-15/app.js
+++ day-15/app.js
@@ -1,12 +1,29 @@
 //Exercises Level 1
 //Create an Animal class. The class will have name, age, color, legs properties and create different methods
 class Animal {
-    constructor(name, age, color, legs) {
-        name = this.name,
-            age = this.age,
-            color = this.color,
-            legs = this.legs
+    constructor(type, name, age, color, legs) {
+        this.name = name,
+            this.age = age,
+            this.color = color,
+            this.legs = legs
+        this.type = type
     }
-
+    getDetails() {
+        console.log(`this is a / an ${this.type}, his/ her name is ${this.name}, he is ${this.age} years old , he has a beautiful ${this.color} color`);
+    };
+    getLegs() {
+        console.log(this.legs);
+    }
 }
 
+const dog = new Animal("dog", "poalo", 3, "black", 4);
+
+console.log(dog);
+dog.getDetails();
+dog.getLegs();
+
+//Create a Dog and Cat child class from the Animal Class.
+
+const cat = new Animal("cat", "santa", 2, "white", 4)
+console.log(cat)
+cat.getDetails();
