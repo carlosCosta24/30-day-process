78b6c84f5051d8c8274ed5c9d8c5319f03c85481
M0lt0 <bodymgahed@yahoo.com>
2024-02-05 19:13:46 -0300

    day 18 l 2 correction

--- day-18/app.js
+++ day-18/app.js
@@ -3,25 +3,36 @@ const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 
 //Exercises: Level 1
 //Read the countries API using fetch and print the name of country, capital, languages, population and area.
-// fetch(countriesAPI).then(response => response.json()).then(
-//     data => {
-//         console.log(data);
-//         for (const ele of data) {
-//             console.info(`name: ${ele.name} , area: ${ele.area} , population: ${ele.population}`);
-//         }
-//     }
-// ).catch(error => console.error(error));
+fetch(countriesAPI).then(response => response.json()).then(
+
+    data => {
+        setTimeout(() => {
+
+            console.log(data);
+            for (const ele of data) {
+                console.info(`name: ${ele.name} , area: ${ele.area} , population: ${ele.population}`);
+            }
+        }, 500)
+    }
+
+
+).catch(error => console.error(error));
 
 //Exercises: Level 2
 //Print out all the cat names in to catNames variable.
 console.log("/-*/*-/*-/-*/-*/-*/-*/-*/-*/-*/-");
-fetch(catsAPI).then(response => response.json()).then(
-    data => {
-        let catNames = [];
-        for (const ele of data) {
-            catNames.push(ele.name);
+fetch(catsAPI).then(response => response.json()).then(data => {
+
+    setTimeout(() => {
+        {
+            let catNames = [];
+            for (const ele of data) {
+                catNames.push(ele.name);
+            }
+            console.log(catNames);
         }
-        console.log(catNames);
-    }
+    }, 600)
+}
+
 ).catch(error => console.error(error));
 
