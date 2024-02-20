aba2a28c57133296d94423640a36fe9d22c0d087
M0lt0 <bodymgahed@yahoo.com>
2024-02-20 15:35:56 -0300

    day 21 mini project

--- day-21/mini project/app.js
+++ day-21/mini project/app.js
@@ -13,23 +13,69 @@ const colorArray = [
     "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke",
     "yellow", "yellowgreen"
 ]
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 const parentList = document.querySelector("ul")
 const year = document.getElementsByClassName("year")
-const list = document.querySelector("li")
+const list = document.querySelectorAll("li")
+const body = document.querySelector("body")
+const mainTitle = document.querySelector("h2")
+
+
+body.style.display = "flex"
+body.style.flexDirection = "row"
+body.style.justifyContent = "center"
+mainTitle.style.display = "flex"
+mainTitle.style.justifyContent = "center"
+mainTitle.style.textDecoration = "underline"
 
 setInterval(() => { year[0].style.color = colorArray[Math.floor(Math.random() * 49)] }, 1000)
-
-year[0].style.fontSize = "60px"
+setInterval(() => { dateAndTime.style.backgroundColor = colorArray[Math.floor(Math.random() * 49)] }, 1000)
 
 //The date and time background color is changing every on seconds
 parentList.style.listStyle = "none"
-list.style.textDecoration = "underline"
-list.style.fontSize = "30px"
 
 const date = new Date()
 const dateAndTime = document.createElement("div")
-dateAndTime.textContent = `${date.getMonth() + 1}`
-parentList.appendChild(dateAndTime)
+dateAndTime.textContent = months[date.getMonth()] + " " + date.getFullYear().toString().slice(2) + " " + date.getUTCFullYear() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+
+mainTitle.style.display = "flex"
+mainTitle.style.flexDirection = "column"
+mainTitle.style.alignItems = "center"
+dateAndTime.style.textDecoration = "none !important"
+mainTitle.appendChild(dateAndTime)
+
+//Completed challenge has background green
+//Ongoing challenge has background yellow
+//Coming challenges have background red
+for (let o = 0; o < list.length; o++) {
+
+    list[o].style.display = "flex"
+    list[o].style.gap = "10px"
+    list[o].style.margin = "10px"
+
+
+    if (list[o].textContent.includes("Done")) {
+        list[o].style.backgroundColor = "green"
+
+    } else if (list[o].textContent.includes("Ongoing")) {
+        list[o].style.backgroundColor = "yellow"
+    } else {
+        list[o].style.backgroundColor = "red"
+    }
+}
 
 
 
