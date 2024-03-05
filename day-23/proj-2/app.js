4df37967521ba4f504cbce1ded1357f92a166a2c
M0lt0 <bodymgahed@yahoo.com>
2024-03-04 23:20:46 -0300

    day-23 proj-2 main style

--- day-23/proj-2/app.js
+++ day-23/proj-2/app.js
@@ -4,6 +4,43 @@ const screen = document.querySelector(".screen")
 const codeArea = document.querySelector(".screen-code")
 
 body.addEventListener("keypress", (e) => {
-    screen.textContent = `you pressed  ${e.key}`
+
+    screen.textContent = `you pressed  ${e.code}`
+
     codeArea.textContent = `${e.keyCode}`
+
 })
+
+{
+    body.style.display = "flex"
+    body.style.flexDirection = "column"
+    body.style.flexWrap = "wrap"
+    body.style.alignContent = "center"
+    body.style.justifyContent = "center"
+    body.style.alignItems = "center"
+}
+{
+    screen.style.display = "flex"
+    screen.style.width = "200px"
+    screen.style.flexDirection = "row"
+    screen.style.alignContent = "center"
+    screen.style.alignItems = "center"
+    screen.style.justifyContent = "center"
+    screen.style.flexWrap = "wrap"
+    screen.style.height = "200px"
+    screen.style.border = "4px solid #ed2323"
+    screen.style.borderRadius = "20px"
+}
+
+{
+    codeArea.style.display = "flex"
+    codeArea.style.width = "80px"
+    codeArea.style.height = "80px"
+    codeArea.style.border = "4px solid green"
+    codeArea.style.borderRadius = "20px"
+    codeArea.style.alignItems = "center"
+    codeArea.style.justifyContent = "center"
+    codeArea.style.margin = "10px"
+    codeArea.style.padding = "10px"
+
+}
