3709eb4de07a174fec606f3676fb687283d36492
M0lt0 <bodymgahed@yahoo.com>
2024-03-05 17:41:16 -0300

    project main structure

--- day-24/app.js
+++ day-24/app.js
@@ -1,3 +1,19 @@
 const inputArea = document.querySelector(".input-field")
 const optionList = document.querySelector(".option-list")
 const button = document.querySelector(".action-button")
+const resultSection = document.querySelector(".result-section")
+const options = document.querySelectorAll("option")
+
+
+function massCalculation() {
+    let value = parseInt(inputArea.value)
+    if (isNaN(value)) {
+        resultSection.textContent = "please enter a number "
+
+
+    } else {
+
+
+
+    }
+}
