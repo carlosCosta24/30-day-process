67cab212376ac59929d11033a25ec537306e8046
M0lt0 <bodymgahed@yahoo.com>
2024-05-08 04:55:46 -0300

    day-29 functionality

--- day-29/app.js
+++ day-29/app.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container')
 const sentence = document.querySelector('.sentence')
 const fancyColors = ["#FFD700", "#FF1493", "#00FFFF", "#BA55D3", "#FF4500", "#9400D3", "#00CED1", "#ADFF2F", "#FF6347", "#8A2BE2", "#32CD32", "#4169E1", "#FF8C00", "#800080", "#20B2AA"];
 const fancyFonts = ["Montserrat", "Roboto Condensed", "Lobster", "Pacifico", "Oswald", "Raleway", "Playfair Display", "Open Sans", "Poppins", "Cormorant Garamond", "Quicksand", "Merriweather", "Amatic SC", "Ubuntu", "Cinzel"];
-const fancyFontSizes = ["48px", "56px", "64px", "72px", "80px", "96px", "120px"];
+const fancyFontSizes = ["64px", "72px", "80px", "96px", "120px"];
 
 
 let animation = () => {
